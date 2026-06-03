27fcc14c78e09396d06bc0ba81eee9772d44d172
krzysztofpipiora-ops <krzysztof.pipiora@gmail.com>
2026-06-03 17:34:19 +0200

    Create commitlint.config.js

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'ci',
+        'build',
+        'revert'
+      ]
+    ],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never']
+  }
+}
