0133f68bf6daf88d761116e63dcd645e2bb0c029
krzysztofpipiora-ops <krzysztof.pipiora@gmail.com>
2026-06-03 18:00:01 +0200

    Create auth.ts

---
+++ src/lib/auth.ts
@@ -0,0 +1,32 @@
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { NextAuthOptions } from 'next-auth'
+import TwitchProvider from 'next-auth/providers/twitch'
+import GoogleProvider from 'next-auth/providers/google'
+import { prisma } from '@/lib/prisma'
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    TwitchProvider({
+      clientId: process.env.TWITCH_CLIENT_ID || 'mock_id',
+      clientSecret: process.env.TWITCH_CLIENT_SECRET || 'mock_secret',
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || 'mock_id',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || 'mock_secret',
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (token && session.user) {
+        // @ts-ignore
+        session.user.id = token.sub
+      }
+      return session
+    },
+  },
+}
