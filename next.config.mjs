bb18077c49e5dc9a6e5896ba8e6fe5eb8339ef85
krzysztofpipiora-ops <krzysztof.pipiora@gmail.com>
2026-06-04 11:44:28 +0200

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Jeśli masz tutaj jakieś inne specyficzne ustawienia (np. images), możesz je tu zostawić
-};
-
-export default nextConfig;
