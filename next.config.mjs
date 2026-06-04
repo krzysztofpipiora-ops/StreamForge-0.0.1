4f4b9f24fdbfc132f857227bc7addb1ed2455a6f
krzysztofpipiora-ops <krzysztof.pipiora@gmail.com>
2026-06-04 11:38:48 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Jeśli masz tutaj jakieś inne specyficzne ustawienia (np. images), możesz je tu zostawić
 };
 
 export default nextConfig;
